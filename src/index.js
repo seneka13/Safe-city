c06b440bebb545afb3d935cac4fd15cd61816fb1
Sam M <62692646+seneka13@users.noreply.github.com>
2020-05-11 19:38:22 +0600

    create main content

---
+++ src/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $( ".nav-btn" ).click(function(){
+        $('.nav-content').slideToggle()
+        
+        
+        // css({'transform':'translateX(0px)', 'opacity':'1'})
+    })
+})
