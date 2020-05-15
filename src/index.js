04624dc294044893b5547d544c1850fb0bb42b14
Sam M <62692646+seneka13@users.noreply.github.com>
2020-05-15 18:32:04 +0600

    style fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
+
 $(document).ready(function(){
-
-
-    $( ".nav-btn" ).click(function(){
+    if($(window).width() < 768) {
+        $( ".nav-btn" ).click(function(){
         $('.nav-content').slideToggle()
+    });
+    }
+    
+
+    $('.order-btn').click(function(){
+        if($(window).width() > 992) {
+            Swal.fire({
+            icon: 'success',
+            title: 'Спасибо, заявка отправлена.',
+            text: 'В ближайшее время мы свяжемся с Вами для уточнения деталей.',
+            confirmButtonColor: "#61c50f",
+            timer: 2000,
+            width: '300px'
+            })
+        } else {
+            Swal.fire({
+                icon: 'success',
+                title: 'Спасибо, заявка отправлена.',
+                text: 'В ближайшее время мы свяжемся с Вами для уточнения деталей.',
+                confirmButtonColor: "#61c50f",
+                timer: 2000,
+                })
+        }
+        
+        setTimeout(() => {
+            $('.close').trigger('click')
+        }, 2000);
+        
     })
 })
 
