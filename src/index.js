6e8efb103e3e641e0818fda58e4d5a8dc6ed6c9e
Sam M <62692646+seneka13@users.noreply.github.com>
2020-05-17 15:26:55 +0600

    business page

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
+$(document).ready(function () {
 
-$(document).ready(function(){
-    if($(window).width() < 768) {
-        $( ".nav-btn" ).click(function(){
-        $('.nav-content').slideToggle()
-    });
-    }
-    
-
-    $('.order-btn').click(function(){
-        if($(window).width() > 992) {
-            Swal.fire({
-            icon: 'success',
-            title: 'Спасибо, заявка отправлена.',
-            text: 'В ближайшее время мы свяжемся с Вами для уточнения деталей.',
+    $('.sweet').click(function () {
+        Swal.fire({
+            showCancelButton: true,
+            title: 'Оставить отзыв',
+            html: '<input id="swal-input1" class="swal2-input" placeholder = "Выше имя..">',
+            input: "textarea",
+            inputPlaceholder: "Оставтье ваш отызыв",
             confirmButtonColor: "#61c50f",
-            timer: 2000,
-            width: '300px'
+            cancelButtonColor: "gray",
+            confirmButtonText: 'Отправить',
+            cancelButtonText: 'Отменить'
+        }).then((result) => {
+            if (result.value) {
+                Swal.fire({
+                    text: 'Ваш отзыв успешно отправлен. После проверки администратором, он будет размещен на сайте!',
+                    icon: 'success',
+                    confirmButtonColor: "#61c50f"
+                })
+            }
+        })
+    })
+
+
+    if ($(window).width() < 768) {
+        $(".nav-btn").click(function () {
+            $('.nav-content').slideToggle()
+        });
+    }
+
+
+    $('.order-btn').click(function () {
+        if ($(window).width() > 992) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Спасибо, заявка отправлена.',
+                text: 'В ближайшее время мы свяжемся с Вами для уточнения деталей.',
+                confirmButtonColor: "#61c50f",
+                timer: 2000,
+                width: '300px'
             })
         } else {
             Swal.fire({
@@ -24,14 +47,12 @@ $(document).ready(function(){
                 text: 'В ближайшее время мы свяжемся с Вами для уточнения деталей.',
                 confirmButtonColor: "#61c50f",
                 timer: 2000,
-                })
+            })
         }
-        
+
         setTimeout(() => {
             $('.close').trigger('click')
         }, 2000);
-        
+
     })
 })
-
-
