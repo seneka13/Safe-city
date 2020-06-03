096a5ef48608ea12da46e911f51cb4bec7dd3503
Sam M <62692646+seneka13@users.noreply.github.com>
2020-06-03 12:44:21 +0600

    scroll

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 $(document).ready(function () {
 
+
+    $('.scroll').on('click', function (e) {
+        $('html,body').stop().animate({
+            scrollTop: $('#about_us').offset().top
+        }, 1000);
+        e.preventDefault();
+    });
+
     $('.send-review-btn').click(function () {
-        $('.reviewForm').each(function(i, item) {
+        $('.reviewForm').each(function (i, item) {
             if (!item.value) {
                 Swal.fire({
                     icon: 'error',
@@ -9,28 +17,29 @@ $(document).ready(function () {
                     confirmButtonColor: "#01A7C2",
                     timer: 2000,
                     width: '300px'
-                })} else {
-                    Swal.fire({
-                        text: 'Ваш отзыв успешно отправлен. После проверки администратором, он будет размещен на сайте!',
-                        icon: 'success',
-                        confirmButtonColor: "#01A7C2",
-                        timer: 2000,
-                    })
-                }
-                setTimeout(() => {
-                    $('.close').trigger('click')
-                }, 2000);
-        }) 
+                })
+            } else {
+                Swal.fire({
+                    text: 'Ваш отзыв успешно отправлен. После проверки администратором, он будет размещен на сайте!',
+                    icon: 'success',
+                    confirmButtonColor: "#01A7C2",
+                    timer: 2000,
+                })
+            }
+            setTimeout(() => {
+                $('.close').trigger('click')
+            }, 2000);
+        })
     })
 
-    
+
     $(".nav-btn").click(function () {
         $('.nav-content').slideToggle()
     });
 
 
     $('.order-btn').click(function () {
-        $('#exampleFormControlInput1').each(function(i, item) {
+        $('#exampleFormControlInput1').each(function (i, item) {
             if (!item.value) {
                 Swal.fire({
                     icon: 'error',
@@ -68,25 +77,16 @@ $(document).ready(function () {
 
 
 
-    $('.contact-btn').click(function(){
+    $('.contact-btn').click(function () {
         Swal.fire({
-            html: 
-            '<p>Адресс: город Бишкек, ул. Горького, 142</p>'+
-            '<p>Email: safe.kg@mail.ru</p>'+
-            '<p>Skype: anna.mdn</p>'+
-            '<p>тел: +996(312) 90-88-07</p>'+
-            '<p>факс: +996(312) 90-11-20</p>'+
-            '<p>моб: +996(707) 12-00-40</p>'+
-            '<p>моб: +996(555) 92-78-80</p>',
+            html: '<p>Адресс: город Бишкек, ул. Горького, 142</p>' +
+                '<p>Email: safe.kg@mail.ru</p>' +
+                '<p>Skype: anna.mdn</p>' +
+                '<p>тел: +996(312) 90-88-07</p>' +
+                '<p>факс: +996(312) 90-11-20</p>' +
+                '<p>моб: +996(707) 12-00-40</p>' +
+                '<p>моб: +996(555) 92-78-80</p>',
             confirmButtonColor: "#01a7c2red"
         })
     })
-
-    $('a [href*="#about_us"]').click(function() {
-        $page.animate({
-            scrollDown: $($.attr(this, 'href'))
-        .offset().down
-        }, 400);
-        return false;
-    });
 })
